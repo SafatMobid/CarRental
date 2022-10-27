d4810dfbad81cf74c0d268373ba2ae9c7557b4a1
Safat Mobid <safatmobid@gmail.com>
2022-10-27 13:29:11 -0400

    Completed Javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,39 @@
 "use strict";
 
 window.onload = init;
+
+function init() {
+    const calculateBtn = document.getElementById("calculateBtn");
+    calculateBtn.onclick = calculateBtnClick;
+}
+
+function calculateBtnClick() {
+    let numberdaysField = Number(document.getElementById("numberdaysField").value);
+    let tollTag = document.getElementById("tollTag").checked;
+    let gpsTag = document.getElementById("gpsTag").checked;
+    let roadsideTag = document.getElementById("roadsideTag").checked;
+    let yesPress = document.getElementById("yesPress").checked;
+
+    let total = numberdaysField*29.99;
+    let extraPerDay = 0;
+
+    if (tollTag) {
+        extraPerDay += 3.95 * numberdaysField;
+    }
+    if (gpsTag) {
+        extraPerDay += 2.95 * numberdaysField;
+    }
+    if (roadsideTag) {
+        extraPerDay += 2.95 * numberdaysField;
+    }
+    let extraTotal = (total + extraPerDay);
+
+    if (yesPress) {
+        extraTotal += total * 0.3
+    }
+
+
+let totalCost = document.getElementById("totalCost");
+totalCost.innerHTML = `Total Cost: $${extraTotal.toFixed(2)}`;
+
+}
